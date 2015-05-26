44e7aa8946a64d1620d598adf26b96bf2e2bf0ef
Joseph Livecchi <joewashear007@gmail.com>
2015-05-25 21:44:10 -0400

    Added a setup script

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+setup(name='ScrappyDoo',
+      version='0.1.0',
+      description='An converstion script to take Sweetshop downloads and put them into CM',
+      author='Joseph Livecchi',
+      author_email='joewashear007@gmail.com',
+      url='https://github.com/joewashear007/ScrappyDoo/',
+      packages=['scrappydoo'],
+      )
